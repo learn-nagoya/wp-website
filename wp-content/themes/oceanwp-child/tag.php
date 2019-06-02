22b1d88246e61620544693f5b5c882ea6cdc5fe4
wykhuh <wykhuh@users.noreply.github.com>
2019-06-02 09:01:30 -0700

    customize tag and search result layouts

---
+++ wp-content/themes/oceanwp-child/tag.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+get_header(); ?>
+
+	<?php do_action( 'ocean_before_content_wrap' ); ?>
+
+	<div id="content-wrap" class="container clr">
+
+		<?php do_action( 'ocean_before_primary' ); ?>
+
+		<div id="primary" class="content-area clr">
+
+			<?php do_action( 'ocean_before_content' ); ?>
+
+			<div id="content" class="site-content clr">
+				<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+
+				<?php do_action( 'ocean_before_content_inner' ); ?>
+
+				<?php if ( have_posts() ) : ?>
+
+						<?php while ( have_posts() ) : the_post(); ?>
+
+							<?php get_template_part( 'partials/search/layout' ); ?>
+
+						<?php endwhile; ?>
+
+					<?php oceanwp_pagination(); ?>
+
+				<?php else : ?>
+
+					<?php
+					// Display no post found notice
+					get_template_part( 'partials/none' ); ?>
+
+				<?php endif; ?>
+
+				<?php do_action( 'ocean_after_content_inner' ); ?>
+
+			</div><!-- #content -->
+
+			<?php do_action( 'ocean_after_content' ); ?>
+
+		</div><!-- #primary -->
+
+		<?php do_action( 'ocean_after_primary' ); ?>
+
+	</div><!-- #content-wrap -->
+
+	<?php do_action( 'ocean_after_content_wrap' ); ?>
+
+<?php get_footer(); ?>

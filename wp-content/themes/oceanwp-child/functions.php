18cd1253701e5b854a7e035a2d1d4fbf345bb1aa
wykhuh <wykhuh@users.noreply.github.com>
2019-07-26 12:15:48 -0500

    dashboard: allow contributers to upload files

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -73,3 +73,15 @@ function hide_counts($views) {
  return $views;
 }
 
+/**********
+ allow contributor to upload files
+**********/
+
+
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+add_action('admin_init', 'allow_contributor_uploads');
+ 
+function allow_contributor_uploads() {
+     $contributor = get_role('contributor');
+     $contributor->add_cap('upload_files');
+}
